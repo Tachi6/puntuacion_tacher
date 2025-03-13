46130898843708f045479a87f7501ff9a9e1705d
David Gonzalez <dav6wat@gmail.com>
2025-03-13 23:54:54 +0100

    Add elevation to snackbar

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -8,6 +8,7 @@ class NotificationServices {
     final snackBar = SnackBar(
       padding: const EdgeInsets.symmetric(vertical: 19),
       duration: const Duration(milliseconds: 2500),
+      elevation: 1,
       content: Text(
         message.toUpperCase(), 
         textAlign: TextAlign.center,
@@ -15,7 +16,6 @@ class NotificationServices {
     );
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    // messengerKey.currentState!.showSnackBar(snackBar);
   }
 
   static showFlushBar(String message, BuildContext context) {
@@ -23,15 +23,18 @@ class NotificationServices {
     final colors = Theme.of(context).colorScheme;
     final styles = Theme.of(context).textTheme;
 
-    return Flushbar(
-      padding: const EdgeInsets.symmetric(vertical: 19),
-      duration:  const Duration(milliseconds: 2500),
-      backgroundColor: colors.inverseSurface,
-      messageText: Text(
-        message.toUpperCase(), 
-        textAlign: TextAlign.center,
-        style: styles.bodyMedium!.copyWith(color: colors.onInverseSurface),
-      ),
-    )..show(context);
+    return Material(
+      elevation: 1,
+      child: Flushbar(
+        padding: const EdgeInsets.symmetric(vertical: 19),
+        duration:  const Duration(milliseconds: 2500),
+        backgroundColor: colors.inverseSurface,
+        messageText: Text(
+          message.toUpperCase(), 
+          textAlign: TextAlign.center,
+          style: styles.bodyMedium!.copyWith(color: colors.onInverseSurface),
+        ),
+      )..show(context),
+    );
   }
 }
